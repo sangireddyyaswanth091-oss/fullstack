954a46da06672e56b4cead04ee4768bcc1c39c3b
sangireddyyaswanth091-oss <sangireddyyaswanth091@gmail.com>
2026-04-20 00:21:24 +0530

    Add files via upload

---
+++ task5/config.py
@@ -0,0 +1,6 @@
+class Config:
+    SECRET_KEY = 'dev_secret_key'
+   # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Viswasanju21!@localhost/student_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/dbname'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///students.db'
